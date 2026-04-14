3e98d442b6d3fbb53ff7ddd1224be65ec0b0b3bf
onik-999 <onikdas.dev@gmail.com>
2026-04-14 20:27:46 +0600

    added navbar and implemented page routing

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FaFacebookF, FaYoutube } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
+
+const Footer = () => {
+    return (
+          <div>
+            this is Footer page
+          </div>
+    );
+};
+
+export default Footer;
