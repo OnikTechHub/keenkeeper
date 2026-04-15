0f2ecad78ab6cb85f890946d4937f7d5f2428721
onik-999 <onikdas.dev@gmail.com>
2026-04-15 22:17:36 +0600

    Added toast notifications

--- src/components/Homepage/AllFriends/FriendDetails/FriendDetails.jsx
+++ src/components/Homepage/AllFriends/FriendDetails/FriendDetails.jsx
@@ -7,42 +7,44 @@ import { MdArchive, MdDelete } from 'react-icons/md';
 import { FaPhoneAlt, FaRegCommentDots, FaVideo } from 'react-icons/fa';
 import { FriendProvider } from '../../../../Contexts/Contexts';
 
+
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const FriendDetails = () => {
     const { id } = useParams()
-    console.log(id)
     const { friends, loading } = useData()
     const data = friends.find((friend) => friend.id == id)
-    // console.log(data)
-    const {call,setCall,text,setText,video,setVideo} = useContext(FriendProvider)
 
-    
+    const { call, setCall, text, setText, video, setVideo } = useContext(FriendProvider)
 
-    const handleCall=()=>{
-        setCall([...call,data])
+    const handleCall = () => {
+        setCall([...call, data])
+        toast.success( ` Call with ${data.name} added to timeline!`)
     }
 
-    const handleText=()=>{
-        setText([...text,data])
+    const handleText = () => {
+        setText([...text, data])
+        toast.success(` Text with ${data.name} added to timeline!`)
     }
 
-    const handleVideo=()=>{
-        setVideo([...video,data])
+    const handleVideo = () => {
+        setVideo([...video, data])
+        toast.success(` Video with ${data.name} added to timeline!`)
     }
-    
-
-
-
 
     if (loading) return (
         <div className='flex justify-center items-center h-64'>
             <RiseLoader color='#244D3F' />
         </div>
     );
+
     return (
-
-
-
         <div className=' bg-gray-50 '>
+
+           
+            <ToastContainer position="top-right" autoClose={2000} />
+
             <div className="w-8/12 mx-auto py-20 grid grid-cols-1 md:grid-cols-3 gap-6">
 
                 {/* LEFT */}
@@ -61,17 +63,13 @@ const FriendDetails = () => {
                         </button>
                     </div>
 
-
                     <div className="flex gap-2 my-3 justify-center">
                         {
                             data.tags.map((tag, i) => <span key={i} className="px-3 py-1 bg-green-100 text-green-700 text-xs font-semibold rounded-full uppercase tracking-wider">
                                 {tag}
                             </span>)
                         }
-
                     </div>
-
-
 
                     <p className="text-sm text-gray-500 mt-3 italic">
                         {data.bio}
@@ -80,7 +78,6 @@ const FriendDetails = () => {
                     <h2 className="text-1xl text-gray-400 mt-1"> {data.email}</h2>
 
                     <div className="mt-6 space-y-2 ">
-
                         <button className="w-full flex items-center  justify-center gap-2 bg-gray-100  rounded-lg p-2 my-3 hover:bg-gray-200">
                             <BsClock /> Snooze 2 Weeks
                         </button>
@@ -92,7 +89,6 @@ const FriendDetails = () => {
                         <button className="w-full flex items-center justify-center gap-2 text-red-500 bg-gray-100 p-2 rounded-lg hover:bg-red-100">
                             <MdDelete /> Delete
                         </button>
-
                     </div>
                 </div>
 
@@ -100,7 +96,6 @@ const FriendDetails = () => {
                 <div className="md:col-span-2 space-y-6">
 
                     <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-
                         <div className="bg-white p-4 rounded-lg shadow text-center">
                             <h2 className="text-2xl font-bold text-green-700">{data.days_since_contact}</h2>
                             <p className="text-sm text-gray-500">Days Since Contact</p>
@@ -117,10 +112,8 @@ const FriendDetails = () => {
                             </h2>
                             <p className="text-sm text-gray-500">Next Due</p>
                         </div>
-
                     </div>
 
-                    {/* RELATIONSHIP GOAL */}
                     <div className="bg-white p-5 rounded-lg shadow flex justify-between items-center">
                         <div>
                             <h3 className="font-medium text-gray-700">Relationship Goal</h3>
@@ -154,7 +147,7 @@ const FriendDetails = () => {
 
                             <button onClick={handleVideo} className="flex flex-col items-center justify-center bg-gray-100 p-6 rounded-lg hover:bg-gray-200">
                                 <FaVideo />
-                                <span  className="mt-2 text-sm">Video</span>
+                                <span className="mt-2 text-sm">Video</span>
                             </button>
 
                         </div>
@@ -164,11 +157,7 @@ const FriendDetails = () => {
 
             </div>
         </div>
-
-
     )
 };
 
 export default FriendDetails;
-
-
